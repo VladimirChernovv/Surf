312f94e70d40d813c849eb092e105fa4c3e97c99
Vladimir Chernov <dgudo73@mail.ru>
2019-11-07 14:42:07 +0300

    Свёрстана секция Travel. Вёртска всё ещё без адаптива

--- app/js/main.js
+++ app/js/main.js
@@ -30,5 +30,12 @@ $(function(){
 		asNavFor: '.surf-slider',
 		focusOnSelect: true,
 	});
+
+	$('.travel__slider').slick ({
+		infinite: true,
+		fade: true,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="arrow">',
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="arrow">',
+	});
   
 });
